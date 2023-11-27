a30b7055fceb753acf8a4bce6bf287fd62b7c781
Héricles Emanuel <hericles.me@icloud.com>
2023-11-27 20:55:31 -0300

    feat: add landing page

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -11,43 +11,23 @@ export interface Props {
   title?: string;
   /** @format textarea */
   headline?: string;
-  links?: Array<Link>;
+  cta?: string;
 }
 
-export default function Hero({
-  title = "deco.cx",
-  logo = "/logo.svg",
-  headline =
-    "The digital experience platform that combines performance and personalization for the ultimate sales results.",
-  links = [
-    { title: "Official website", "href": "https://deco.cx/" },
-    { title: "Linkedin", "href": "https://www.linkedin.com/company/deco-cx/" },
-    { title: "Discord", "href": "https://deco.cx/discord" },
-  ],
-}: Props) {
+export default function Hero({ title, logo, headline, cta }: Props) {
   return (
-    <header class="lg:container mx-8 md:mx-16 lg:mx-auto mt-8 md:mt-12 mb-28 text-xl md:text-base">
-      <div class="mb-10 md:mb-20">
-        <img
-          class="object-cover w-20"
-          src={logo}
-          alt={title}
-        />
+    <div class="flex justify-center items-center hero min-h-screen bg-[#FAFAFA] text-[#313639]">
+      <div class="hero-overlay bg-opacity-60"></div>
+      <div class="hero-content text-center text-neutral-content">
+        <div class="max-w-lg space-y-10">
+          <img src={logo} alt="retale logo" class="mx-auto w-80" />
+          <div>
+            <h1 class="mb-2 text-3xl font-bold">{title}</h1>
+            <p>{headline}</p>
+          </div>
+          <button class="hover:scale-105 duration-200 py-2 px-4 rounded-full font-bold text-[#fff] bg-gradient-to-r from-[#02F67C] to-[#3FB8A1]">{cta}</button>
+        </div>
       </div>
-      <div class="font-bold text-3xl lg:text-6xl leading-tight lg:leading-none xl:w-5/6">
-        {headline}
-      </div>
-      {!!links?.length && (
-        <ul class="mt-8 flex flex-col md:flex-row gap-2 md:gap-4">
-          {links.map(({ href, title }) => (
-            <li>
-              <a target="_blank" href={href} aria-label={title} class="link">
-                {title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      )}
-    </header>
+    </div>
   );
 }
